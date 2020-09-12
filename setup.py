f11dd3c68d45c619a32f2ff81e087bf7315e335f
Marcell Pek <marcell.pek@marcellp.net>
2020-09-13 00:53:50 +0100

    fix

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biomage-utils",
+    version="0.0.1",
+    author="Biomage Ltd.",
+    author_email="hello@biomage.net",
+    description="A CLI package for managing Biomage infrastructure and codebase.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/biomage-ltd/biomage-utils",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    entry_points = {
+        'console_scripts': ['biomage=biomage.cli:main'],
+    }
+)
