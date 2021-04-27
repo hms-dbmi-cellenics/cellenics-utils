83e70c2aff69befbfbe0ab2ef1958ef0236909bf
Pol Alvarez <pol.avms@gmail.com>
2021-04-27 15:00:35 +0200

    Modified pull experiment to get also cell sets (#17)
    * modified pull experiment to get also cell sets
    
    * saving cellsets in plain text, fixed equality detection for dynamoDB files

--- biomage/experiment/pull.py
+++ biomage/experiment/pull.py
@@ -1,29 +1,47 @@
 import boto3
 import click
-from deepdiff import DeepDiff
 
 from .utils import (
     PULL,
     Summary,
-    download_S3_obj,
+    download_S3_json,
+    download_S3_rds,
     is_modified,
     load_cfg_file,
     save_cfg_file,
 )
 
+CELLSETS_FILE = "mock_cell_sets.json"
+PLOTS_TABLES_FILE = "mock_plots_tables.json"
+EXPERIMENTS_FILE = "mock_experiment.json"
+SAMPLES_FILE = "mock_samples.json"
+RDS_FILE = "r.rds"
 
-def download_if_modified(bucket, key):
+SAMPLES_TABLE = "samples"
+EXPERIMENTS_TABLE = "experiments"
+
+
+def download_S3_obj(s3_obj, key, filepath):
+    if RDS_FILE in filepath:
+        download_S3_rds(s3_obj, key, filepath)
+    elif CELLSETS_FILE in filepath:
+        download_S3_json(s3_obj, key, filepath)
+    else:
+        raise ValueError(f"unexpected file: {filepath}")
+
+
+def download_if_modified(bucket, key, filepath):
     s3 = boto3.resource("s3")
     s3_obj = s3.Object(bucket, key)
 
-    if is_modified(s3_obj, key):
+    if is_modified(s3_obj, filepath):
         click.echo(
             f"Local file for key {key} last modified date "
             "differs from S3 version.\n Updating local copy"
         )
 
-        download_S3_obj(s3_obj, key)
-        Summary.add_changed_file(key + ".gz")
+        download_S3_obj(s3_obj, key, filepath)
+        Summary.add_changed_file(filepath)
 
 
 def remove_key(dic, k):
@@ -35,10 +53,13 @@ def remove_key(dic, k):
     return dic
 
 
-def update_config_if_needed(file, table_name, experiment_id):
+def update_config_if_needed(filepath, table_name, experiment_id):
+    """
+    Filepath: experiment_id/filename
+    """
     dynamodb = boto3.resource("dynamodb")
 
-    local_cfg, found = load_cfg_file(file)
+    local_cfg, found = load_cfg_file(filepath)
     remote_cfg = dynamodb.Table(table_name).get_item(
         Key={"experimentId": experiment_id}
     )["Item"]
@@ -52,16 +73,16 @@ def update_config_if_needed(file, table_name, experiment_id):
         remote_cfg = remove_key(remote_cfg, "pipeline")
 
     # if the local config was not found or it's different from the remote => update
-    if not found or DeepDiff(local_cfg, remote_cfg):
-        save_cfg_file(remote_cfg, file)
-        Summary.add_changed_file(file)
+    if not found or local_cfg != remote_cfg:
+        save_cfg_file(remote_cfg, filepath)
+        Summary.add_changed_file(filepath)
 
 
 def update_configs(experiment_id, origin):
     # config pairs like: (local file name, remote table name)
     configs = [
-        ("mock_experiment.json", "experiments"),
-        ("mock_samples.json", "samples"),
+        (EXPERIMENTS_FILE, EXPERIMENTS_TABLE),
+        (SAMPLES_FILE, SAMPLES_TABLE),
     ]
 
     for file_name, table_name in configs:
@@ -72,9 +93,9 @@ def update_configs(experiment_id, origin):
     # plots and tables config has key issues (references that do no
     # exist locally), for now just create an empty json
     empty_plots_tables = {"records": []}
-    filename = "mock_plots_tables.json"
-    save_cfg_file(empty_plots_tables, filename)
-    Summary.add_changed_file(filename)
+    filepath = f"{experiment_id}/{PLOTS_TABLES_FILE}"
+    save_cfg_file(empty_plots_tables, filepath)
+    Summary.add_changed_file(filepath)
 
 
 @click.command()
@@ -99,10 +120,17 @@ def pull(experiment_id, origin):
     """
 
     Summary.set_command(cmd=PULL, origin=origin, experiment_id=experiment_id)
-    bucket = f"biomage-source-{origin}"
-    file = f"{experiment_id}/r.rds"
 
-    download_if_modified(bucket, file)
+    bucket = f"biomage-source-{origin}"
+    file = f"{experiment_id}/{RDS_FILE}"
+    dst_file = file + ".gz"
+    download_if_modified(bucket=bucket, key=file, filepath=dst_file)
+
+    bucket = f"cell-sets-{origin}"
+    dst_file = f"{experiment_id}/{CELLSETS_FILE}"
+
+    # the name of the cell sets file in S3 is just the experiment ID
+    download_if_modified(bucket=bucket, key=experiment_id, filepath=dst_file)
 
     update_configs(experiment_id, origin)
 
