f21fd35b506a62ca58bdb38bfa565649505acc16
Pol Alvarez <pol.avms@gmail.com>
2021-05-14 12:56:37 +0200

    missing constants file

---
+++ biomage/utils/constants.py
@@ -0,0 +1,19 @@
+# Constant names of environments
+DEVELOPMENT = "development"
+STAGING = "staging"
+PRODUCTION = "production"
+
+# Default values used by bimoage-utils
+DEFAULT_SANDBOX = "default"
+DEFAULT_EXPERIMENT_ID = "e52b39624588791a7889e39c617f669e"
+
+# Names of mock files & dataset used to feed inframock
+CELLSETS_FILE = "mock_cell_sets.json"
+PLOTS_TABLES_FILE = "mock_plots_tables.json"
+EXPERIMENTS_FILE = "mock_experiment.json"
+SAMPLES_FILE = "mock_samples.json"
+RDS_FILE = "r.rds"
+
+# DynamoDB table names
+SAMPLES_TABLE = "samples"
+EXPERIMENTS_TABLE = "experiments"
