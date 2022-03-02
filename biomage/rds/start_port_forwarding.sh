6758afa49ed2f4a242d9b55ad3a1f4b902d69fe4
cosa65 <martin@biomage.net>
2022-03-02 16:04:53 -0300

    Add vars to start_port_forwarding bash

--- biomage/rds/start_port_forwarding.sh
+++ biomage/rds/start_port_forwarding.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+ENVIRONMENT=$1
+LOCAL_PORT=$2
+
 RDSHOST="$(aws rds describe-db-cluster-endpoints \
 	--region eu-west-1 \
-	--db-cluster-identifier aurora-cluster-$1 \
+	--db-cluster-identifier aurora-cluster-$ENVIRONMENT \
 	--filter Name=db-cluster-endpoint-type,Values='writer' \
 	--query 'DBClusterEndpoints[0].Endpoint' \
 	| tr -d '"')"
 
 INSTANCE_DATA=$(aws ec2 describe-instances \
-	--filters "Name=tag:Name,Values=rds-$1-ssm-agent" \
+	--filters "Name=tag:Name,Values=rds-$ENVIRONMENT-ssm-agent" \
 	--output json \
 	--query "Reservations[*].Instances[*].{InstanceId:InstanceId, AvailabilityZone:Placement.AvailabilityZone}")
 
@@ -18,7 +21,7 @@ ssh-keygen -t rsa -f temp -N ''
 
 aws ec2-instance-connect send-ssh-public-key --instance-id $INSTANCE_ID --availability-zone $AVAILABILITY_ZONE --instance-os-user ssm-user --ssh-public-key file://temp.pub
 
-ssh -i temp -N -f -M -S temp-ssh.sock -L "$2:${RDSHOST}:5432" "ssm-user@${INSTANCE_ID}" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o ProxyCommand="aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters portNumber=%p"
+ssh -i temp -N -f -M -S temp-ssh.sock -L "$LOCAL_PORT:${RDSHOST}:5432" "ssm-user@${INSTANCE_ID}" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o ProxyCommand="aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters portNumber=%p"
 
 echo "Finished setting up, run \"biomage rds login\" in a different tab"
 echo 
