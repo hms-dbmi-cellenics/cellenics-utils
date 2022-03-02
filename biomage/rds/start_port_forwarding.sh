7429e6c1c1bd1ad26e4a565214793118445bd6dc
cosa65 <martin@biomage.net>
2022-03-02 17:03:45 -0300

    Add cleanup to trap

--- biomage/rds/start_port_forwarding.sh
+++ biomage/rds/start_port_forwarding.sh
@@ -2,6 +2,12 @@
 ENVIRONMENT=$1
 LOCAL_PORT=$2
 
+function cleanup() {
+	ssh -O exit -S temp-ssh.sock *
+	rm temp*
+	echo "Finished cleaning up"
+}
+
 RDSHOST="$(aws rds describe-db-cluster-endpoints \
 	--region eu-west-1 \
 	--db-cluster-identifier aurora-cluster-$ENVIRONMENT \
@@ -21,6 +27,8 @@ ssh-keygen -t rsa -f temp -N ''
 
 aws ec2-instance-connect send-ssh-public-key --instance-id $INSTANCE_ID --availability-zone $AVAILABILITY_ZONE --instance-os-user ssm-user --ssh-public-key file://temp.pub
 
+trap cleanup EXIT
+
 ssh -i temp -N -f -M -S temp-ssh.sock -L "$LOCAL_PORT:${RDSHOST}:5432" "ssm-user@${INSTANCE_ID}" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o ProxyCommand="aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters portNumber=%p"
 
 echo "Finished setting up, run \"biomage rds login\" in a different tab"
@@ -30,7 +38,3 @@ echo "Press any key to close session."
 echo "------------------------------"
 read
 echo
-
-ssh -O exit -S temp-ssh.sock *
-
-rm temp*
