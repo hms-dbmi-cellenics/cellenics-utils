c499e937590d0908a97cf94c7809b5bd8748b79e
cosa65 <martin@biomage.net>
2022-03-04 09:12:34 -0300

    Cleanup rds.py

--- biomage/rds/rds.py
+++ biomage/rds/rds.py
@@ -1,13 +1,5 @@
-# import itertools
-from pprint import pprint
-
-# import boto3
 import click
-from botocore.exceptions import ClientError
-from deepdiff import DeepDiff
-from PyInquirer import prompt
 
-from ..utils.constants import DEVELOPMENT, PRODUCTION, STAGING
 from .login import login
 from .tunnel import tunnel
 
