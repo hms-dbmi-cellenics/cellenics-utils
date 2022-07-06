4c239bd68a68b1ba45cb1873296c79ffd6da3e2f
Anugerah Erlaut <aerlaut@live.com>
2022-07-06 17:09:25 +0100

    strip and lower email

--- biomage/account/account.py
+++ biomage/account/account.py
@@ -222,6 +222,7 @@ def create_users_list(user_list, header, input_env, overwrite):
         df = pd.read_csv(user_list, header=header, quoting=csv.QUOTE_ALL)
         for _, full_name, email in df.itertuples():
 
+            email = email.strip().lower()
             error = _validate_input(email, full_name)
             if error:
                 print(error)
@@ -230,7 +231,6 @@ def create_users_list(user_list, header, input_env, overwrite):
             password = generate_password()
 
             full_name = full_name.title()
-            email = email.lower()
 
             error = _create_user(full_name, email, password, userpool, overwrite)
             if error and not ("UsernameExistsException" in str(error) and overwrite):
